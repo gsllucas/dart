1ac58300fdabffd0f68a433ba980f2acb2ad3bd2
gsllucas <lucas.goncalves@coopersystem.com.br>
2021-12-18 17:16:25 -0300

    :boom: working with exceptions on dart, it throws a created custom exception if the login is failed

---
+++ exceptions.dart
@@ -0,0 +1,35 @@
+void main() {
+  try {
+    validateLogin(username: 'myUserName@123', password: 123);
+  } on UserLoginException catch (error) {
+    error.message != null ? print(error.message) : print(error);
+  } catch (error) {
+    print(error);
+  } finally {
+    print('End');
+  }
+}
+
+validateLogin({required String username, required dynamic password}) {
+  Map<String, dynamic> fakeDataBase = {
+    'username': 'myUserName@123',
+    'password': '123456'
+  };
+
+  if (username == fakeDataBase['username'] &&
+      password == fakeDataBase['password']) {
+    print('Login realizado com sucesso!');
+  } else {
+    throw UserLoginException();
+  }
+}
+
+class UserLoginException implements Exception {
+  final String? message;
+
+  UserLoginException({String? this.message});
+
+  @override
+  String toString() =>
+      'Erro ao realizar o seu login, por favor verifique suas credenciais.';
+}
